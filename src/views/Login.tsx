3797ffd89e54deb16ec94c81a3ffdfbf700aac8a
Daniel Bonifacio <contato@danielbonifacio.com.br>
2020-07-31 13:23:03 -0300

    feature(login): form

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Container from '../shared/Container'
+import Form from '../shared/Form'
+import Input from '../shared/Input'
+import { Grid, Button } from '@material-ui/core'
 
 const LoginView: React.FC = () => {
+  const [credentials, setCredentials] = useState({ user: '', pass: '' })
+
+  const handleFormSubmit = () => {
+    console.log(credentials)
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setCredentials({
+      ...credentials,
+      [name]: value
+    })
+  }
+
   return <div>
     <Header title="Login: Alga Stock" />
     <Container>
-      Login
+      <Form title="Login" onSubmit={handleFormSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Input
+              fullWidth
+              onChange={handleInputChange}
+              value={credentials.user}
+              name="user"
+              label="Username"
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Input
+              fullWidth
+              onChange={handleInputChange}
+              value={credentials.pass}
+              name="pass"
+              label="Password"
+              type="password"
+              required
+            />
+          </Grid>
+          <Grid item xs={12} justify="flex-end">
+            <Button type="submit" color="primary" variant="contained" >
+              Login
+            </Button>
+          </Grid>
+        </Grid>
+        
+        
+      </Form>
     </Container>
   </div>
 }
